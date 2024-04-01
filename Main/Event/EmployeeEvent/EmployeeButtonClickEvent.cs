2dcc4c65a2c6f0856255cd2473b13068e1f3329d
GE77 <skyriyvrzo@gmail.com>
2024-04-01 15:45:58 +0700

    4165165

---
+++ Main/Event/EmployeeEvent/EmployeeButtonClickEvent.cs
@@ -0,0 +1,42 @@
+﻿namespace Main.Event.EmployeeEvent
+{
+#pragma warning disable IDE1006
+    internal class EmployeeButtonClickEvent
+    {
+        /*
+         * Clear all text in textbox
+         */
+
+        internal static void onClickAdd()
+        {
+            
+        }
+
+        /*
+         * Enable textbox to editable
+         * 
+         */
+        internal static void onClickEdit()
+        {
+
+        }
+
+        /*
+         * Delete data from table.
+         *
+         * Show MessageBox confirm when pressing delete button
+         */
+        internal static void onClickDelete()
+        {
+
+        }
+
+        /*
+         * Save data into database
+         */
+        internal static void onClickSave()
+        {
+
+        }
+    }
+}
---
+++ Main/Event/EmployeeEvent/EmployeeStartupEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Main.Event.EmployeeEvent
+{
+    #pragma warning disable IDE1006
+    internal class EmployeeStartupEvent
+    {
+        internal static void onStartup()
+        {
+
+        }
+    }
+}
